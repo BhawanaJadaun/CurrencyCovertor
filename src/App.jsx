66841fd43a97f87a7048e96f081526b084fcc152
BhawanaJadaun <bhawanajadaun0001@gmail.com>
2024-06-08 22:06:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'; // Import App.css
+import CurrencyConvertor from './Components/CurrencyConvertor';
+
+function App() {
+  return (
+    <div className='w-full min-h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat   ' 
+      
+  >
+    <div className='container'>
+      <CurrencyConvertor/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
